12e2a5331c53dab1a84610ca0fdba370d8f7c7e9
debashisneog23 <debashisneog23@gmail.com>
2023-11-26 20:08:52 +0530

    order status

--- src/pages/Checkout/Checkout.js
+++ src/pages/Checkout/Checkout.js
@@ -51,6 +51,7 @@ const Checkout = () => {
             cartTotalAmount,
             userDetails,
             userID,
+            status: 'processing',
             createdAt: Timestamp.now().toDate()
         };
         try{
